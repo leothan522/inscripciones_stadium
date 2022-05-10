3f4573f1d5590ae071991159b6f99a58337bc0eb
Yonathan Castillo <leothan522@gmail.com>
2022-05-10 08:55:58 -0400

    Footer Web

--- resources/views/web/layouts/footer.blade.php
+++ resources/views/web/layouts/footer.blade.php
@@ -1,8 +1,11 @@
 <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-        Anything you want
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 1.0
     </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>&copy; 2022 | {{ config('app.name') }}
+        | Ing. Yonathan Castillo.
+    </strong>
+    <em class="text-sm">
+        Basado en <a href="http://adminlte.io" target="_blank">AdminLTE.io</a>.
+    </em>
 </footer>
